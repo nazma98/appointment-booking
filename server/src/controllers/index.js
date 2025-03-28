8abb2a7094eac71fee04a5a4f76d463a5bc343b5
Fariha Promi <farihapromi2611@gmail.com>
2025-03-28 21:43:22 +0600

    Update index

--- server/src/controllers/index.js
+++ server/src/controllers/index.js
@@ -3,3 +3,4 @@ export * as signupController from './signup.js';
 export * as slotController from './slot.js';
 export * as currentUserController from './currentUser.js';
 export * as roleController from './roleController.js';
+export * as appointmentController from './appointment.js';
