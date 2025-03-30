3b793e0d1bc7eedb34e0a6dd3228daf3edb7fcdb
nazma98 <nazmasarker1311@gmail.com>
2025-03-30 12:59:07 +0600

    Add slotTable component and route

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -22,21 +22,20 @@ createRoot(document.getElementById('root')!).render(
     <QueryProvider>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<HomePage />}>
-            <Route path='/book-slot' element={<BookSlotPage />} />
+          <Route path="/" element={<HomePage />}>
+            <Route path="/book-slot" element={<BookSlotPage />} />
           </Route>
-          <Route path='/login' element={<LoginPage />} />
-          <Route path='/signup' element={<SignupPage />} />
-          <Route path='admin' element={<AdminPageLayout />}>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="admin" element={<AdminPageLayout />}>
             <Route index element={<Dashboard />} />
-            <Route path='slots' element={<Slots />}>
-              <Route path='edit/:userId' element={<SlotTable />} />
+            <Route path="slots" element={<Slots />}>
+              <Route path="edit" element={<SlotTable />} />
             </Route>
-            <Route path='appointments' element={<Appointments />} />
+            <Route path="appointments" element={<Appointments />} />
           </Route>
         </Routes>
       </BrowserRouter>
     </QueryProvider>
   </StrictMode>
 );
-
