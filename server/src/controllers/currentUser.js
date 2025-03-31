be8a40458aea7f08ddee86d59d434e4a03cd7027
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-03-31 23:43:07 +0600

    get user profile in current user api

--- server/src/controllers/currentUser.js
+++ server/src/controllers/currentUser.js
@@ -3,7 +3,7 @@ import { logger } from '../config/logger.js';
 import { getCurrentUserById } from '../services/index.js';
 
 export const getcurrentUser = asyncHandler(async (req, res) => {
-  try {
+  
     const userId = req.user._id;
     // const userId = req.query._id ; //when there is no authentication setup
     if (!userId) {
@@ -12,6 +12,20 @@ export const getcurrentUser = asyncHandler(async (req, res) => {
       });
     }
     const user = await getCurrentUserById(userId);
+    if (!user){
+      return res.status(404).json({
+        success: false,
+        message: 'User not found',
+      });  
+    }
+     if(user.error){
+    logger.error('Error fetching current user:', user.error.message);
+    return res.status(500).json({
+      success: false,
+      message: 'Failed to fetch user details',
+      error: user.error.message,
+    });
+  }
     res.status(200).json({
       id: user._id,
       email: user.email,
@@ -22,14 +36,11 @@ export const getcurrentUser = asyncHandler(async (req, res) => {
           description: user.role.description,
           permissions: user.role.permissions
       } : null,
-      profile: user.profile,
+      profile: user.profile? {
+        id: profile._id,
+        image: profile.image,
+        address: profile.address,
+        dateOfBirth: profile.dateOfBirth
+      } : null
     });
-  } catch (error) {
-    logger.error('Error fetching current user:', error.message);
-    res.status(500).json({
-      success: false,
-      message: 'Failed to fetch user details',
-      error: error.message,
-    });
-  }
-});
+  });
--- server/src/services/currentUser.js
+++ server/src/services/currentUser.js
@@ -1,12 +1,11 @@
-import { User } from '../models/index.js';
+import { User, UserProfile } from '../models/index.js';
 export const getCurrentUserById = async (userId) => {
-  try {
+
     const currentUser = await User.findById(userId).select('-password').populate({path: 'role',select: 'role description permissions'});
     if (!currentUser) {
       throw new Error('User not found');
     }
-    return currentUser;
-  } catch (error) {
-    throw error;
-  }
+    const currentUserProfile = await UserProfile.findOne({user: userId,
+      deleted: false })
+    return {currentUser, currentUserProfile};
 };
