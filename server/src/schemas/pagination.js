eb59a0cba3897e60481c98491f233539f6e924b9
thanwin84 <thanwinhline84@gmail.com>
2025-04-03 05:29:12 +0600

    Add paginatin schema

---
+++ server/src/schemas/pagination.js
@@ -0,0 +1,8 @@
+import z from 'zod';
+
+export const paginationSchema = z.object({
+  hits: z.array(z.any()),
+  total: z.number().nonnegative({ message: 'Total can not be non negative' }),
+  page: z.number().min(1, 'Page number must be at least 1'),
+  limit: z.number().min(1, 'Limit must be at least 1'),
+});
