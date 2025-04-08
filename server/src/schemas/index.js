5a28c4389abc54078cf0f796861226ad6cfd6154
Fariha Promi <farihapromi2611@gmail.com>
2025-04-08 15:00:47 +0600

    Update pagination schema

--- server/src/schemas/index.js
+++ server/src/schemas/index.js
@@ -3,4 +3,4 @@ export * from './slot.js';
 export * from './role.js';
 export * from './appointment.js';
 export * from './userProfile.js';
-
+export * from './pagination.js';
--- server/src/schemas/pagination.js
+++ server/src/schemas/pagination.js
@@ -1,8 +1,22 @@
 import z from 'zod';
 
 export const paginationSchema = z.object({
-  hits: z.array(z.any()),
+  hits: z.array(z.any()).optional(),
   total: z.number().nonnegative({ message: 'Total can not be non negative' }),
   page: z.number().min(1, 'Page number must be at least 1'),
   limit: z.number().min(1, 'Limit must be at least 1'),
 });
+
+export const formatPagination = ({ total, page, limit }) => {
+  paginationSchema.parse({
+    total,
+    page,
+    limit,
+  });
+
+  return {
+    total,
+    page,
+    limit,
+  };
+};
