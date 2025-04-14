7d7d3a966f686068af1c3642b0aa0a1bdb8e594a
thanwin84 <thanwinhline84@gmail.com>
2025-04-14 16:07:32 +0600

    Add Custom error class

--- server/src/utils/customErrors.js
+++ server/src/utils/customErrors.js
@@ -6,3 +6,36 @@ export class DuplicateResourceError extends Error {
     Error.captureStackTrace(this, this.constructor);
   }
 }
+export class NotFoundError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = this.constructor.name;
+    this.status = 404;
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+export class BadRequest extends Error {
+  constructor(message) {
+    super(message);
+    this.name = this.constructor.name;
+    this.status = 400;
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+export class UnauthorizedError extends Error {
+  constructor(message = 'Unauthorized') {
+    super(message);
+    this.name = this.constructor.name;
+    this.status = 401;
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+export class ForbiddenError extends Error {
+  constructor(message = 'Forbidden') {
+    super(message);
+    this.name = this.constructor.name;
+    this.status = 403;
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
