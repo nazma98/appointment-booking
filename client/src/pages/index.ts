67615e8d7929b0d6584354b76449fd8d24981844
Armstrong104 <ameertalha22@gmail.com>
2025-04-25 19:52:35 +0600

    Remove admin route from index.ts

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -1,7 +1,6 @@
 export * from './HomePage';
 export * from './BookSlotPage';
 export * from './SignupPage';
-export * from './admin';
 export * from './UnauthorizePage';
 export { default as LoginPage } from './LoginPage';
 export { default as RootLayout } from './RootLayout';
